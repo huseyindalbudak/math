d30b6fc44a1048df0b5c3f6ef961f91c87aa3b98
huseyin dalbudak <93huseyin93@gmail.com>
2018-03-03 23:29:34 +0300

    Update monterand.py

--- monterand.py
+++ monterand.py
@@ -2,18 +2,18 @@
 
 import numpy as np 
 
-inside = 0
-tot = 1000
+def montecarlo_rectangular(area,totaldot)
+  inside = 0
+  
+  for i in range(0, tot):
+    # Generate random x, y in [0, 1].
+    x = np.random.rand()
+    y = np.random.rand()
+      # Increment if inside unit circle.
+    if (x*y) <area:     
+        inside += 1
 
-for i in range(0, tot):
-  # Generate random x, y in [0, 1].
-  x = np.random.rand()
-  y = np.random.rand()
-    # Increment if inside unit circle.
-  if (x*y) <0.9:     # 0.9 is area wanted
-      inside += 1
+  square = float(inside) / total
 
-square = float(inside) / total
-
-# It works!
-print(square)
+  # It works!
+  return square
